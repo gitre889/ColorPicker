8cab93686a7e0b54fdef7182534ec2d2e7929a0e
gitre889 <145955634+gitre889@users.noreply.github.com>
2023-09-25 15:21:06 +0900

    Add files via upload

---
+++ javascript_color/script.js
@@ -0,0 +1,9 @@
+//console.log(document.getElementById('myColor').value);
+
+//定数の宣言
+const color = document.getElementById('myColor');
+const text = document.getElementById('bodyText');
+
+//document.getElementById('bodyText').textContent =document.getElementById('myColor').value;
+
+text.textContent = color.value;
